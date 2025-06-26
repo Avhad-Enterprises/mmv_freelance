de2df6998ddeeb51eeac576d501ba43cf259ff85
avhadenterprisespc7 <avhadenterprisespc7@gmail.com>
2025-06-26 14:26:02 +0530

    fix: remove tracked ignored files from repository

---
+++ backend/src/dtos/applied_projects.dto.ts
@@ -0,0 +1,53 @@
+import {
+    IsBoolean,
+    IsEnum,
+    IsInt,
+    IsOptional,
+    IsString,
+    IsDateString,
+} from 'class-validator';
+
+export class AppliedProjectsDto {
+    @IsOptional({ groups: ['update'] })
+    @IsInt({ groups: ['update'] })
+    applied_projects_id?: number;
+
+    @IsInt({ groups: ['create', 'update'] })
+    projects_task_id: number;
+
+    @IsInt({ groups: ['create', 'update'] })
+    users_id: number;
+
+    @IsOptional({ groups: ['create', 'update'] })
+    @IsInt({ groups: ['create', 'update'] })
+    status?: number;
+
+    @IsOptional({ groups: ['create', 'update'] })
+    @IsBoolean({ groups: ['create', 'update'] })
+    is_active?: boolean;
+
+    @IsOptional({ groups: ['create', 'update'] })
+    @IsBoolean({ groups: ['create', 'update'] })
+    is_deleted?: boolean;
+
+    @IsOptional({ groups: ['create', 'update'] })
+    @IsInt({ groups: ['create', 'update'] })
+    deleted_by?: number;
+
+    @IsOptional({ groups: ['create', 'update'] })
+    @IsInt({ groups: ['create', 'update'] })
+    created_by?: number;
+
+    @IsOptional({ groups: ['create', 'update'] })
+    @IsInt({ groups: ['create', 'update'] })
+    updated_by?: number;
+
+    @IsOptional()
+    @IsDateString()
+    created_at?: string;
+
+    @IsOptional()
+    @IsDateString()
+    updated_at?: string;
+}
+  

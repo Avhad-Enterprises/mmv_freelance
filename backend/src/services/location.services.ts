a9f4025e6bfad1535aff7c487291b7ca67cfc8aa
Avhad-123 <avhadenterprisespc5@gmail.com>
2025-08-14 11:06:42 +0530

    mmv-freelance

--- backend/src/services/location.services.ts
+++ backend/src/services/location.services.ts
@@ -25,6 +25,17 @@ class locationService {
         return cities;
     }
 
+    public async getStatesBy(country_id: number): Promise<any> {
+        if (!country_id) throw new HttpException(400, 'Country ID is required');
+        const states = await DB(T.STATES).where({ country_id });
+        return states;
+    }
+    
+    public async getCitiesBy(state_id: number): Promise<any> {
+        if (!state_id) throw new HttpException(400, 'State ID is required');
+        const cities = await DB(T.CITIES).where({ state_id });
+        return cities;
+    }
 
 }
 export default locationService;
